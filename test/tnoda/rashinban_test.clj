8832db632c2d1ba1863a5634f87820c3cd6322fd
Takahiro Noda <takahiro.noda+github@gmail.com>
2015-05-18 18:16:59 +0900

    Add minimal test

--- test/tnoda/rashinban_test.clj
+++ test/tnoda/rashinban_test.clj
@@ -1,7 +1,23 @@
 (ns tnoda.rashinban-test
   (:require [clojure.test :refer :all]
-            [tnoda.rashinban :refer :all]))
+            [clojure.test.check :as tc]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [tnoda.rashinban :as r]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defspec a-number-array-is-converted-into-an-array-of-doubles
+  100
+  (prop/for-all [xs (gen/vector gen/int)]
+    (= (seq (map double xs)) (apply r/c xs))))
+
+(defspec boolean-values-support
+  100
+  (prop/for-all [bs (gen/vector gen/boolean)]
+    (= (seq bs) (apply r/c bs))))
+
+(defspec string-values-support
+  100
+  (prop/for-all [ss (gen/vector gen/string-alphanumeric)]
+    (= (seq ss) (apply r/c ss))))
+
