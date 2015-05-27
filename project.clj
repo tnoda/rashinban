e764dadb67592becb0dc4d7f7cbeedd68bebf7d2
Takahiro Noda <takahiro.noda+github@gmail.com>
2015-05-28 00:35:51 +0900

    Add dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [incanter "1.5.6"]
                  [org.rosuda.REngine/Rserve "1.8.1"]
-                 [org.rosuda.REngine/REngine "2.1.0"]]
+                 [org.rosuda.REngine/REngine "2.1.0"]
+                 [incanter "1.5.6"
+                  :exclusions [org.clojure/clojure]]
+                 [clatrix "0.4.0"
+                  :exclusions [org.clojure/clojure]]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.7.0"]]}})
