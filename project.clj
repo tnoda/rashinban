ba7c8e82a7d53bbf872e305c9331892bfd298392
Takahiro Noda <takahiro.noda+github@gmail.com>
2015-06-23 21:46:18 +0900

    Add vectorz-clj to dependencies

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.rosuda.REngine/Rserve "1.8.1"]
                  [org.rosuda.REngine/REngine "2.1.0"]
-                 [net.mikera/core.matrix "0.36.1"]]
+                 [net.mikera/core.matrix "0.36.1"]
+                 [net.mikera/vectorz-clj "0.30.1"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.7.0"]]}})
