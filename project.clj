a7d7df2186c279e97a1ed4c7858e813e6f08eac4
Takahiro Noda <takahiro.noda+github@gmail.com>
2015-06-26 15:55:49 +0900

    Remove dependencies on core.matrix

--- project.clj
+++ project.clj
@@ -4,7 +4,5 @@
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.rosuda.REngine/Rserve "1.8.1"]
-                 [org.rosuda.REngine/REngine "2.1.0"]
-                 [net.mikera/core.matrix "0.36.1"]
-                 [net.mikera/vectorz-clj "0.30.1"]]
+                 [org.rosuda.REngine/REngine "2.1.0"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.7.0"]]}})
