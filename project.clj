a9e6053d2a9cf852d04d594f52e03e045415a2e7
Takahiro Noda <takahiro.noda+github@gmail.com>
2015-08-25 17:57:26 +0900

    Bump to 0.0.155

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tnoda/rashinban "0.0.154"
+(defproject org.clojars.tnoda/rashinban "0.0.155"
   :description "A Clojure library to work with R"
   :url "https://github.com/tnoda/rashinban"
   :license {:name "Apache License, Version 2.0"
