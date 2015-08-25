e22a00721df3bb736622d91cf18795b14ac59fba
Takahiro Noda <takahiro.noda+github@gmail.com>
2015-08-25 17:56:36 +0900

    Upgrade the test.check version to 0.8.0

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.rosuda.REngine/Rserve "1.8.1"]
                  [org.rosuda.REngine/REngine "2.1.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.7.0"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
+                                  [org.clojure/test.check "0.8.0"
+                                   :exclusions [org.clojure/clojure]]]}})
